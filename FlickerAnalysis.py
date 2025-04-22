9a544ccc806003d6400a0688280c4744608e9ac3
Brandon Coventry <bscoventry@gmail.com>
2025-04-22 10:10:18 -0500

    Updated Flicker Analysis

--- FlickerAnalysis.py
+++ FlickerAnalysis.py
@@ -27,7 +27,7 @@ def read_16bit_signed_binary(file_path):
     return data
 
 # Example usage:
-file_path = 'SSVEP_MKIII.bin'  # Replace with your file path
+file_path = 'F1.bin'  # Replace with your file path
 data = read_16bit_signed_binary(file_path)
 
 #print(data)
@@ -36,7 +36,8 @@ data = read_16bit_signed_binary(file_path)
 #plt.show()
 plt.plot(data)
 plt.show()
-srange1 = (1650000,1760000)#(30770,35822) #Video 1: (134000,233800) Video 2:(344756,444022) Video 3: (563682,663680) Video 4: (780000,900000) Video 5: (1000000,1120000) Video 6: (1225010,1324300) Video 7: (1425000,1540000) Video 8: (1650000,1760000)
+srange1 = (52000,153000)#F1#(37000,135700)#E2#(47000,147000)#D1#(45000,144000)#C1#(28000,128000)#A2#(147000,346800)#A1#(44000,144000)#H4#(34000,135000)#H2#(45280,146000)#H1
+#srange1 = (1650000,1760000)#(30770,35822) #Video 1: (134000,233800) Video 2:(344756,444022) Video 3: (563682,663680) Video 4: (780000,900000) Video 5: (1000000,1120000) Video 6: (1225010,1324300) Video 7: (1425000,1540000) Video 8: (1650000,1760000)
 Flicker = data#df['Channel 1 (V)'].values
 Flick1 = Flicker[srange1[0]:srange1[1]]
 plt.plot(Flick1)
